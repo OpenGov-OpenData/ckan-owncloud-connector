fa064ca48cedd08633db0a948a19547b1d6d5e05
Brandon Buchthal <brandon@buchthal.com>
2017-10-17 16:02:13 -0400

    updated main

--- pythonFiles/Main.py
+++ pythonFiles/Main.py
@@ -1,9 +1,7 @@
 import json
 import os
 import shutil
-
 import sys
-
 import owncloud
 import urllib3
 import ckan_file_upload
@@ -16,8 +14,12 @@ urllib3.disable_warnings()
 
 class TestFileAccess(object):
     def __init__(self):
-        self.client = owncloud.Client(config.Config['owncloud_url'], dav_endpoint_version=0)
-        self.client.login(config.Config['owncloud_login'], config.Config['owncloud_password'])
+        try:
+            self.client = owncloud.Client(config.Config['owncloud_url'], dav_endpoint_version=0)
+            self.client.login(config.Config['owncloud_login'], config.Config['owncloud_password'])
+        except:
+            print('Problems with owncloud connection. Process aborting')
+            sys.exit(0)
 
     def test_upload_directory(self, ownclouddirectory, localdirectory):
         self.client.put_directory(ownclouddirectory + '/', localdirectory + '/')
@@ -42,24 +44,30 @@ def zip_file_extractor(zip_file_path, destination_file_path):
     os.remove(zip_file_path)
 
 
-def file_upload_fail(zip_file, local_upload_directory, orginization, error_message):
-    print(error_message + ' in ' + zip_file.name + '.  Upload failed')
-    f = open(local_upload_directory + '/result.txt', 'w')
-    f.write(error_message + ' in ' + zip_file.name + '.  Upload failed')
-    f.close()
-    shutil.make_archive(local_upload_directory, 'zip', local_upload_directory)
-    ownCloudMethods.test_upload_directory(orginization + '/Failure', local_upload_directory)
-    shutil.rmtree(local_upload_directory)
+def file_upload_fail(zip_file, local_upload_directory, organization_name, error_message):
+    try:
+        print(error_message + ' in ' + zip_file.name + '.  Upload failed')
+        f = open(local_upload_directory + '/result.txt', 'w')
+        f.write(error_message + ' in ' + zip_file.name + '.  Upload failed')
+        f.close()
+        shutil.make_archive(local_upload_directory, 'zip', local_upload_directory)
+        ownCloudMethods.test_upload_directory(organization_name + '/Failure', local_upload_directory)
+        shutil.rmtree(local_upload_directory)
+    except:
+        print('File re-upload failed.  Check ' + organization_name + ' Failure directory')
 
 
-def file_upload_success(zip_file, local_upload_directory, orginization):
-    print(zip_file.name + ' successfully uploaded')
-    f = open(local_upload_directory + '/result.txt', 'w')
-    f.write(zip_file.name + ' successfully uploaded')
-    f.close()
-    shutil.make_archive(local_upload_directory, 'zip', local_upload_directory)
-    ownCloudMethods.test_upload_directory(orginization + '/Success', local_upload_directory)
-    shutil.rmtree(local_upload_directory)
+def file_upload_success(zip_file, local_upload_directory, organization_name):
+    try:
+        print(zip_file.name + ' successfully uploaded')
+        f = open(local_upload_directory + '/result.txt', 'w')
+        f.write(zip_file.name + ' successfully uploaded')
+        f.close()
+        shutil.make_archive(local_upload_directory, 'zip', local_upload_directory)
+        ownCloudMethods.test_upload_directory(organization_name + '/Success', local_upload_directory)
+        shutil.rmtree(local_upload_directory)
+    except:
+        print('File re-upload failed.  Check ' + organization_name + ' Success directory')
 
 
 if __name__ == '__main__':
@@ -67,11 +75,7 @@ if __name__ == '__main__':
     current_timestamp = (time.strftime('%Y%m%d%H%M%S'))
     localDownloadDirectory = config.Config['localDownloadDirectory'] + '/owncloud ' + current_timestamp
 
-    try:
-        OrganizationDirectories = ownCloudMethods.test_get_file_listing('')
-    except:
-        print('Problems with owncloud connection. Process aborting')
-        sys.exit(0)
+    OrganizationDirectories = ownCloudMethods.test_get_file_listing('')
 
     try:
         os.mkdir(localDownloadDirectory)
@@ -100,64 +104,78 @@ if __name__ == '__main__':
             fileListing = os.listdir(localUploadDirectory)
 
             if len(fileListing) != 2:
-                try:
-                    file_upload_fail(zipFile, localUploadDirectory, organization.name, 'Incorrect number of files')
-                except:
-                    print('File re-upload failed.  Check ' + organization.name + ' Failure directory')
+                file_upload_fail(zipFile, localUploadDirectory, organization.name, 'Incorrect number of files')
                 continue
             elif not (os.path.isfile(localUploadDirectory + '/job.json')):
-                try:
-                    file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No job.json file')
-                except:
-                    print('File re-upload failed.  Check Owncloud Failure directory')
+                file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No job.json file')
                 continue
             else:
                 open(localUploadDirectory + '/job.json').read()
                 with open(localUploadDirectory + '/job.json') as data_file:
                     data = json.load(data_file)
 
-                resource_name = data['resource_name']
-                resource_id = data['resource_id']
-                package_id = data['package_id']
-                base_url = data['ckan_url']
-                file_name = data['file_name']
-                api_key = data['ckan_api_key']
-                file_type = data['file_type']
-                resource_dict = {'package_id': data['package_id'], 'name': data['resource_name'], 'url': '',
-                                 'description': data['resource_description'],
+                resource_name = data.get('resource_name')
+                resource_id = data.get('resource_id')
+                package_id = data.get('package_id')
+                base_url = data.get('ckan_url')
+                file_name = data.get('file_name')
+                api_key = data.get('ckan_api_key')
+                file_type = data.get('file_type')
+                resource_dict = {'package_id': data.get('package_id'), 'name': data.get('resource_name'), 'url': '',
+                                 'description': data.get('resource_description'),
                                  }
-                package_dict = {'id': data['package_id']}
                 fullFilePath = localUploadDirectory + '/' + file_name
 
                 if not api_key:
                     print('No API key.  Aborting')
                     file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No API key in job.json')
+                    continue
                 if not resource_name:
                     print('No resource name.  Aborting')
                     file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No resource name in job.json')
+                    continue
                 if not base_url:
                     print('No base url.  Aborting')
                     file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No base url in job.json')
+                    continue
                 if not package_id:
                     print('No package ID.  Aborting')
                     file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No package id in job.json')
+                    continue
                 if not file_type:
                     print('No file type.  Aborting')
                     file_upload_fail(zipFile, localUploadDirectory, organization.name, 'No file type in job.json')
+                    continue
 
-                if not resource_id:
-                    if ckan_file_upload.package_show(base_url, api_key, package_dict):
-                        print localUploadDirectory + '/' + file_name
-                        ckan_file_upload.upload_resource_create(base_url, api_key, resource_dict,
+                if resource_id:
+                    try:
+                        print(ckan_file_upload.upload_resource_patch(base_url, api_key, resource_dict,
+                                                               fullFilePath, resource_name))
+                    except:
+                        file_upload_fail(zipFile, localUploadDirectory, organization.name, 'CKAN file patch failed')
+
+                elif package_id:
+                    try:
+                        print(ckan_file_upload.upload_resource_create(base_url, api_key, resource_dict,
                                                                 fullFilePath,
-                                                                file_name)
-                #     else:
-                #         ckan_file_upload.upload_package()
-                #         ckan_file_upload.upload_resource_create()
-
+                                                                file_name))
+                    except:
+                        file_upload_fail(zipFile, localUploadDirectory, organization.name,
+                                         'CKAN file upload failed')
                 else:
-                    ckan_file_upload.upload_resource_patch(base_url, api_key, resource_dict,
-                                                           fullFilePath, resource_name)
+                    dataset_dict = {
+                        'name': data.get('dataset_name'),
+                        'notes': data.get('dataset_notes'),
+                        'owner_org': data.get('owner_org')
+                    }
+                    try:
+                        print(ckan_file_upload.upload_package(base_url, api_key, dataset_dict))
+                        resource_dict['package_id'] = dataset_dict['name']
+                        print(ckan_file_upload.upload_resource_create(base_url, api_key, resource_dict,
+                                                                fullFilePath, file_name))
+                    except:
+                        file_upload_fail(zipFile, localUploadDirectory, organization.name,
+                                         'CKAN file upload failed')
                 shutil.rmtree(localUploadDirectory, ignore_errors=True)
             shutil.rmtree(localDownloadDirectory + '/' + organization.name, ignore_errors=True)
     shutil.rmtree(localDownloadDirectory, ignore_errors=True)
